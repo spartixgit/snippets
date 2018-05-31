18dafd77f1d60872158fc83476a11a3d8e0015ce
SPARTIX Research <39691921+spartixgit@users.noreply.github.com>
2018-05-31 13:15:40 -1000

    Add files via upload
    Assists with issuing multiple URL requests in a loop with the ability to analyze/process response data and modify url requests in the loop as needed.

---
+++ urllooper.py
@@ -0,0 +1,19 @@
+
+# Name: urllooper.py
+# Description: Code snippet that makes HTTP/HTTPS url requests in a loop and
+# allows you to process the data received and change URL parameters before
+# the next url request.
+
+import urllib.request
+url='http://www.spartix.com'
+
+for i in range(0,3):
+	res = urllib.request.urlopen(url)
+	data = res.read() # can also limit by bytes using res.read(10) to read first 10 bytes
+	# insert code to process the data received
+
+	# if a certain condition is met, you can also change the url for the next reqest	
+	#if i == 1:
+	#	url='https://www.google.com'	
+	print(data)
+	# or do something else with the data
